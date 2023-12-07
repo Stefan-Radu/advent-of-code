87ca9d20cce598ddf7c30dfa14209b7ba74c5363
Stefan Radu <stefan270101@gmail.com>
2023-12-07 15:32:14 +0100

    day1 but nicer

--- 2023/day1/main.rs
+++ 2023/day1/main.rs
@@ -1,64 +1,64 @@
-use std::io::Error;
+fn process_string(s: &str) -> u32 {
+    //let chars: Vec<i32> = s
+        //.chars()
+        //.map(|x: char| x.to_digit(10))
+        //.filter(|opt| x.is_some())
+        //.map(|opt| opt.unwrap())
+        //.collect();
+    //match chars.first() {
+        //Some(elem) => {
+            //(elem - '0' as i32) * 10 
+                //+ (chars.last().unwrap()) - '0' as i32
+        //},
+        //None => 0
+    //}
 
-fn process_string(s: &str) -> i32 {
-    let chars: Vec<i32> = s
+    let chars: Vec<_> = s
         .chars()
-        .filter(|&x| x.is_digit(10))
-        .map(|x: char| x as i32)
+        .filter_map(|x: char| x.to_digit(10))
         .collect();
-    match chars.first() {
-        Some(elem) => {
-            (elem - '0' as i32) * 10 
-                + (chars.last().unwrap()) - '0' as i32
-        },
-        None => 0
-    }
+
+    // this can panic!
+    //let c = chars.get(0); -> gives option
+    chars[0] * 10 + chars[chars.len() - 1]
 }
 
-fn task_1(content: &Result<String, Error>) {
-    match content {
-        Ok(c) => {
-            let res: i32 = c.split('\n')
-                .map(|x| process_string(&x.to_string()))
-                .sum();
-            println!("{}", res);
-        }, 
-        Err(e) => {
-            eprintln!("{e}");
-        }
-    }
+fn task_1(c: &String) {
+    let res: u32 = c.lines()
+        .map(process_string)
+        .sum();
+    println!("{}", res);
 }
 
-fn task_2(content: &Result<String, Error>) {
+fn task_2(c: &String) {
     let numbers: Vec<&str> = vec![
         "zero", "one", "two", "three", "four",
         "five", "six", "seven", "eight", "nine"];
 
-    match content {
-        Ok(c) => {
-            let mut data: String = c.clone();
+    let mut data: String = c.clone();
 
-            for (nr, s) in numbers.iter().enumerate() {
-                let mut cnt = 1;
-                for (idx, _) in data.clone().match_indices(s) {
-                    let ch: char = char::from_digit(nr as u32, 10).unwrap();
-                    data.insert(idx + cnt, ch);
-                    cnt += 1;
-                }
-            }
-
-            task_1(&Ok(data));
-        }, 
-        Err(e) => {
-            eprintln!("{e}");
+    for (nr, s) in numbers.iter().enumerate() {
+        let mut cnt = 1;
+        for (idx, _) in data.clone().match_indices(s) {
+            let ch: char = char::from_digit(nr as u32, 10).unwrap();
+            data.insert(idx + cnt, ch);
+            cnt += 1;
         }
     }
+
+    task_1(&data);
 }
 
 fn main() {
     let input_file_path = "./input.txt";
-    let content = std::fs::read_to_string(input_file_path);
 
-    task_1(&content);
-    task_2(&content);
+    match std::fs::read_to_string(input_file_path) {
+        Ok(content) => {
+            task_1(&content);
+            task_2(&content);
+        }
+        Err(e) => {
+            eprintln!("{e}");
+        }
+    }
 }
