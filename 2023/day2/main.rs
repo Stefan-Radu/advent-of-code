fe231fd79b9539dac536eb8c1fc30328ed7b70fb
Stefan Radu <stefan270101@gmail.com>
2023-12-07 17:41:18 +0100

    day 2 done

--- 2023/day2/main.rs
+++ 2023/day2/main.rs
@@ -1,7 +1,9 @@
 fn parse_batch(s: &str) -> (u32, u32, u32) {
+    // takes a group of extracted cubes
+    // returns the rgb count from the group
     let rgb_color_count: (u32, u32, u32) = s.split(", ")
         .map(|s| {
-            let v: Vec<&str> = s.split(" ").collect();
+            let v: Vec<&str> = s.split(' ').collect();
             let amount: u32 = v[0].parse().unwrap();
             (amount, v[1])
         })
@@ -14,25 +16,32 @@ fn parse_batch(s: &str) -> (u32, u32, u32) {
             }
         });
 
-    rgb_color_count
+    rgb_color_count 
 }
 
-fn is_batch_valid((r, g, b): (u32, u32, u32)) -> bool {
+fn is_batch_valid((r, g, b): &(u32, u32, u32)) -> bool {
+    // valid if values are leq than limits
     let red_limit   = 12;
     let green_limit = 13;
     let blue_limit  = 14;
-    r <= red_limit && g <= green_limit && b <= blue_limit
+    *r <= red_limit && *g <= green_limit && *b <= blue_limit
 }
 
-fn task_1_fully_functional(data: &String) -> u32 {
+fn extract_rgb(line: &str) -> Vec<(u32, u32, u32)> {
+    line.split(": ") // separate game from colors
+        .last().unwrap() // take just the colors
+        .split("; ") // split in batches
+        .map(parse_batch) // extract batch as (r, g, b)
+        .collect() // make Vec
+}
+
+fn task_1(data: &String) {
     let ret: u32 = data.lines().enumerate()
         .map(|(line_idx, line)| {
-            let game_is_valid = line
-                .split(": ") // separate game from colors
-                .last().unwrap() // take just the colors
-                .split("; ") // split in batches
-                .map(parse_batch) // extract batch as (r, g, b)
-                .all(is_batch_valid); // say if the batch is valid
+            // valid if all batches are valid
+            let game_is_valid = extract_rgb(line)
+                .iter()
+                .all(is_batch_valid);
 
             if game_is_valid { 
                 (line_idx + 1) as u32
@@ -41,32 +50,32 @@ fn task_1_fully_functional(data: &String) -> u32 {
             }})
         .sum();
 
-    //println!("{ret}");
-    ret
+    println!("{ret}");
 }
 
-fn task_1_semi_functional(data: &String) -> u32 {
-    let mut ret = 0;
+fn task_2(data: &String) {
+    use std::cmp::max;
 
-    for (game_idx, line) in data.lines().enumerate() {
-        let game_valid = line
-            .split(": ") // separate game from colors
-            .last().unwrap() // take just the colors
-            .split("; ") // split in batches
-            .map(parse_batch) // extract batch as (r, g, b)
-            .all(is_batch_valid); // say if the batch is valid
+    let ret: u32 = data.lines()
+        .map(|line| -> u32 {
+            let (r_limit, g_limit, b_limit) = extract_rgb(line)
+                .iter()
+                .fold((0, 0, 0), | acc, &val | ( 
+                        max(acc.0, val.0),
+                        max(acc.1, val.1),
+                        max(acc.2, val.2),
+                    ));
 
-        ret += if game_valid { (game_idx + 1) as u32 } else { 0 };
-    }
+            r_limit * g_limit * b_limit
+        })
+        .sum();
 
     println!("{ret}");
-    ret
 }
 
 fn main() {
     if let Ok(data) = std::fs::read_to_string("./input.txt") {
-        assert_eq!(
-            task_1_fully_functional(&data),
-            task_1_semi_functional(&data));
+        task_1(&data);
+        task_2(&data);
     }
 }
